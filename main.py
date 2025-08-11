7f8f2e83ae163137e5129f9054682acf5ebbde39
Gabriel Mota <gabriel_mota2001@hotmail.com>
2025-08-11 10:15:50 -0300

    post1.0

---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+from dotenv import load_dotenv
+from mods.sql_server import connect_sql_server, run_query
+from mods.google_sheets import auth_google_sheets, clear_worksheet, insert_dataframe_to_worksheet
+from mods.logger import setup_logger, get_logger
+
+# Define a pasta base (onde o main.py está localizado)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def load_query(file_path: str) -> str:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        query = file.read()
+    return query
+
+def main():
+    # Inicializa o sistema de log
+    setup_logger()
+    logger = get_logger()
+
+    try:
+        logger.info('Iniciando o processo de atualização de dados.')
+
+        # Carrega variáveis de ambiente
+        load_dotenv()
+
+        # Variáveis
+        GOOGLE_URL = os.getenv('URL')
+        GOOGLE_CREDS = os.path.join(BASE_DIR, 'config', 'g_creds.json')
+        CONN_STRING = os.getenv('CONN_STRING')
+        WORKSHEET_NAME = 'MAILING_INADIMPLENTES'
+        QUERY_PATH = os.path.join(BASE_DIR, 'queries', 'inadimplentes.sql')
+
+        logger.info('Carregando query SQL.')
+        SQL_QUERY = load_query(QUERY_PATH)
+
+        logger.info('Conectando ao SQL Server.')
+        conn = connect_sql_server(CONN_STRING)
+
+        logger.info('Executando a consulta.')
+        df = run_query(conn, SQL_QUERY)
+
+        logger.info('Conectando ao Google Sheets.')
+        client = auth_google_sheets(GOOGLE_CREDS)
+
+        logger.info('Formatando datas.')
+        for col in df.select_dtypes(include=["datetime64[ns]"]).columns:
+            df[col] = df[col].dt.strftime("%d/%m/%Y")
+
+        logger.info('Tratando valores ausentes (NaN).')
+        df = df.fillna("")
+
+        logger.info('Limpando a worksheet.')
+        clear_worksheet(client, GOOGLE_URL, WORKSHEET_NAME)
+
+        logger.info('Inserindo novos dados na worksheet.')
+        insert_dataframe_to_worksheet(client, GOOGLE_URL, WORKSHEET_NAME, df)
+
+        logger.info('Processo finalizado com sucesso.')
+
+    except Exception as e:
+        logger.error(f'Erro durante o processo: {e}', exc_info=True)
+        raise  # <- Broker for Jenkins
+
+if __name__ == '__main__':
+    main()
---
+++ queries/inadimplentes.sql
@@ -0,0 +1,232 @@
+WITH HIST AS (
+    SELECT
+        DISTINCT CAST(HS.DATA_ALTERACAO AS DATE) AS DATA_ALTERACAO,
+        HS.CLIENTE_ID,
+        HS.STATUS_ANTERIOR,
+        HS.STATUS_ALTERADO,
+        CASE
+            WHEN HS.STATUS_ANTERIOR = 3
+            AND HS.STATUS_ALTERADO = 33 THEN 'INADIMPLENCIA'
+            WHEN HS.STATUS_ANTERIOR = 2
+            AND HS.STATUS_ALTERADO = 3 THEN 'REATIVACAO'
+            ELSE 'REATIVACAO'
+        END AS ALTERACAO
+    FROM
+        HISTORICO_STATUS HS
+    WHERE
+        HS.STATUS_ALTERADO IN (3, 33)
+        AND HS.STATUS_ANTERIOR IN (2, 3, 33)
+),
+CLUSTERS AS (
+    SELECT
+        CLIENTE_ID,
+        COUNT(*) AS QTD_REATIVACOES,
+        CASE
+            WHEN COUNT(*) <= 1 THEN 'OCASIONAL'
+            WHEN COUNT(*) <= 2 THEN 'MEDIANO'
+            ELSE 'OFENSOR'
+        END AS CLUSTER_ATUAL
+    FROM
+        HIST
+    WHERE
+        DATA_ALTERACAO BETWEEN DATEADD(MONTH, -6, GETDATE())
+        AND GETDATE()
+    GROUP BY
+        CLIENTE_ID
+),
+HIST_COM_CLUSTER AS (
+    SELECT
+        H.*,
+        CASE
+            WHEN H.ALTERACAO = 'INADIMPLENCIA'
+            AND C.CLUSTER_ATUAL IS NULL THEN 'SEM REATIVACAO'
+            ELSE C.CLUSTER_ATUAL
+        END AS CLUSTER_ATUAL
+    FROM
+        HIST H
+        LEFT JOIN CLUSTERS C ON H.CLIENTE_ID = C.CLIENTE_ID
+),
+PARCELAS AS (
+    SELECT
+        CLIENTE_ID,
+        COUNT(*) AS QTD_FATURAS_ABERTO
+    FROM
+        (
+            SELECT
+                CLIENTE_ID
+            FROM
+                FATURAS_CARTAO_CREDITO
+            WHERE
+                STATUS_PROVIDER = 'pending'
+                AND DATA_VENCIMENTO >= DATEADD(MONTH, -4, GETDATE())
+            UNION
+            ALL
+            SELECT
+                CLIENTE_ID
+            FROM
+                FATURAS_CARNE
+            WHERE
+                STATUS_PROVIDER = 'overdue'
+                AND DATA_VENCIMENTO >= DATEADD(MONTH, -4, GETDATE())
+        ) AS FATURAS_ABERTAS
+    GROUP BY
+        CLIENTE_ID
+),
+HIST_INAD AS (
+    SELECT
+        C.ID,
+        C.DATA_INICIO_PLANO,
+        CASE
+            WHEN DATEDIFF(
+                DAY,
+                C.DATA_INICIO_PLANO,
+                CAST(GETDATE() AS DATE)
+            ) < 2 THEN '1 Dia'
+            WHEN DATEDIFF(
+                DAY,
+                C.DATA_INICIO_PLANO,
+                CAST(GETDATE() AS DATE)
+            ) <= 30 THEN CONCAT(
+                DATEDIFF(
+                    DAY,
+                    C.DATA_INICIO_PLANO,
+                    CAST(GETDATE() AS DATE)
+                ),
+                ' Dias'
+            )
+            ELSE CASE
+                WHEN CEILING(
+                    DATEDIFF(
+                        DAY,
+                        C.DATA_INICIO_PLANO,
+                        CAST(GETDATE() AS DATE)
+                    ) / 30.0
+                ) > 12 THEN CONCAT(
+                    FORMAT(
+                        FLOOR(
+                            CEILING(
+                                DATEDIFF(
+                                    DAY,
+                                    C.DATA_INICIO_PLANO,
+                                    CAST(GETDATE() AS DATE)
+                                ) / 30.0
+                            ) / 12.0
+                        ),
+                        'N0',
+                        'pt-BR'
+                    ),
+                    ' Anos'
+                )
+                ELSE CONCAT(
+                    FORMAT(
+                        CEILING(
+                            DATEDIFF(
+                                DAY,
+                                C.DATA_INICIO_PLANO,
+                                CAST(GETDATE() AS DATE)
+                            ) / 30.0
+                        ),
+                        'N0',
+                        'pt-BR'
+                    ),
+                    ' Meses'
+                )
+            END
+        END AS TEMPO_DE_CASA,
+        C.DATA_NASCIMENTO,
+        C.IDADE,
+        CASE
+            WHEN C.IDADE BETWEEN 0
+            AND 18 THEN '0 a 18'
+            WHEN C.IDADE BETWEEN 19
+            AND 34 THEN '19 a 34'
+            WHEN C.IDADE BETWEEN 35
+            AND 55 THEN '35 a 55'
+            WHEN C.IDADE BETWEEN 56
+            AND 75 THEN '56 a 75'
+            WHEN C.IDADE >= 76 THEN '75+'
+            ELSE 'Idade inválida'
+        END AS FAIXA_ETARIA,
+        C.NOME,
+        C.TELEFONE,
+        C.TELEFONE_3,
+        C.TELEFONE_4,
+        c.TELEFONE_FIXO,
+        C.CPF,
+        C.ENDERECO_ID,
+        TRIM(REPLACE(E.CEP, CHAR(9), '')) AS CEP,
+        C.STATUS_CLIENTE_ID,
+        SC.NOME AS STATUS_CLIENTE,
+        C.FORMA_PAGAMENTO_ID,
+        FP.NOME AS FORMA_PAGAMENTO,
+        C.PLANO_ID,
+        P.NOME AS PLANO,
+        CASE
+            WHEN C.TIPO_CONTRATO IS NULL THEN 'Contrato digital'
+            ELSE C.TIPO_CONTRATO
+        END AS TIPO_CONTRATO,
+        C.GENERO,
+        CASE
+            WHEN C.FORMA_PAGAMENTO_ID = 14 THEN ISNULL(
+                NULLIF(C.VALOR_DESCONTO_CARNE, 0),
+                P.MENSALIDADE_CARNE
+            )
+            WHEN C.FORMA_PAGAMENTO_ID = 13 THEN ISNULL(
+                NULLIF(C.VALOR_DESCONTO_CARTAO_CREDITO, 0),
+                P.MENSALIDADE_CARTAO_CREDITO
+            )
+            ELSE NULL
+        END AS DESCONTO_VALOR,
+        CAST(I.DATA_ALTERACAO AS DATETIME) AS DATA_ALTERACAO,
+        I.STATUS_ANTERIOR,
+        I.STATUS_ALTERADO,
+        I.ALTERACAO,
+        I.CLUSTER_ATUAL AS CLUSTER,
+        PC.QTD_FATURAS_ABERTO
+    FROM
+        CLIENTES C
+        LEFT JOIN PLANOS P ON C.PLANO_ID = P.ID
+        LEFT JOIN FORMAS_PAGAMENTO FP ON C.FORMA_PAGAMENTO_ID = FP.ID
+        LEFT JOIN STATUS_CLIENTE SC ON C.STATUS_CLIENTE_ID = SC.ID
+        LEFT JOIN ENDERECOS E ON C.ENDERECO_ID = E.ID
+        LEFT JOIN HIST_COM_CLUSTER I ON C.ID = I.CLIENTE_ID
+        LEFT JOIN PARCELAS PC ON C.ID = PC.CLIENTE_ID
+    WHERE
+        C.STATUS_CLIENTE_ID = 33
+        AND C.FORMA_PAGAMENTO_ID IN (13, 14)
+        AND C.ID NOT IN ('111295', '111302', '111304', '91225')
+),
+HIST_NUMERADO AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY ID
+            ORDER BY
+                DATA_ALTERACAO DESC
+        ) AS RN
+    FROM
+        HIST_INAD
+)
+SELECT
+    DATA_INICIO_PLANO,
+    TEMPO_DE_CASA,
+    DATA_NASCIMENTO,
+    IDADE,
+    FAIXA_ETARIA,
+    NOME,
+    TELEFONE,
+    TELEFONE_3,
+    TELEFONE_4,
+    TELEFONE_FIXO,
+    FORMA_PAGAMENTO,
+    PLANO,
+    GENERO,
+    DESCONTO_VALOR,
+    CLUSTER,
+    QTD_FATURAS_ABERTO
+FROM
+    HIST_NUMERADO
+WHERE
+    RN = 1
+ORDER BY
+    ID;
